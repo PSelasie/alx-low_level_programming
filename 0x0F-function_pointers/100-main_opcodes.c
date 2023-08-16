8d1409bd62b18c769313d362dd1043f2001c99ba
Selasie Pecker <delbertpecker@gmail.com>
2023-08-16 12:48:30 -0700

    advance correction

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,50 +1,38 @@
-
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
 
+/**
+ * main - function with two arguments
+ * @argc: int type argument count
+ * @argv: char type argument array
+ *
+ * Description: print opcode
+ * Return: na
+ */
+int main(int argc, char **argv)
+{
+	int count;
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
+	count = 0;
+	if (argc != 2)
 	{
-		int bytes, i;
-		char *arr;
-
-
-		if (argc != 2)
-		{
-			printf("Error\n");
-			exit(1);
-		}
-
-
-		bytes = atoi(argv[1]);
-
-
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-
-		arr = (char *)main;
-
-
-		for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", arr[i]);
-				break;
-			}
-			printf("%02hhx ", arr[i]);
-		}
-		return (0);
+		printf("Error\n");
+		exit(1);
 	}
-
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (count < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + count));
+		count++;
+		if (atoi(argv[1]) > count)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
